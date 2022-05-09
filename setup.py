6d7a00f19f76627d532d36a5e8c06d8109190360
Joseph Diza <josephm.diza@gmail.com>
2022-05-09 01:23:10 -0700

    Single source package version in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
 from setuptools import find_packages, setup
+import codecs
+import os.path
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    ''' Single source package version in src/package_name/__init__.py '''
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(
     name='sapply',
+    version=get_version("src/sapply/__init__.py"),
     license='MIT',
     author='Joseph Diza',
     author_email='josephm.diza@gmail.com',
