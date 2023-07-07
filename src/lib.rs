7f7d7f824f5aa81fef098fe5ba9a221bf28d2c36
Joseph Diza <josephm.diza@gmail.com>
2023-07-06 22:38:16 -0700

    Import resources in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
-// Include resources
-//include!(concat!(env!("OUT_DIR"), "/resources.rs"));
-
 pub mod app;
-//pub mod resources;
 
-//use resources::*;
+// Include resources
+include!(concat!(env!("OUT_DIR"), "/resources.rs"));
