9daf87e82b584fedcbda461cbeb30af77a580fbf
Joseph Diza <josephm.diza@gmail.com>
2023-07-09 17:52:45 -0700

    Create rev_iter() extension for IndexMap
    It doesn't quite return the iterator properly but it works for now

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
-use std::{fs::File, io::Write, env, path::Path};
+#![feature(type_alias_impl_trait)]
 
-use indexmap::{IndexMap, set::Iter};
+use std::{fs::File, io::Write, env, path::Path, hash::{Hasher, BuildHasher}};
+
+use indexmap::{IndexMap, map::{Iter, IntoIter}};
 use serde_json::Value;
 use phf::phf_map;
 
@@ -59,6 +61,98 @@ where
     write_map(file, name, &map);
 }
 
+//trait IdAssigner<K> {
+    //fn assign_id(mut self, key: K) -> (usize, bool);
+//}
+
+//impl<K> IdAssigner<K> for HashMap<K, usize>
+    //where K: Eq + Hash,
+
+pub trait HashMapReverseIterExt<K,V> {
+    //type Item = (V, K);
+    //type ReverseIterator: Iterator<Item = (V, K)>;
+    //type Item;
+    //type ReverseIterator: Iterator<Item = Self::Item>;
+    //fn rev_iter(self) -> Self;
+    //fn rev_iter<'a>(self) -> Iter<'a, V, K>;
+    //fn rev_iter<'a>(self) -> Iter<'a, V, K>;
+    //fn rev_iter(self) -> IntoIter<V, K>;
+    fn rev_iter(self) -> Self;
+}
+
+//impl<'a, K, V> HashMapReverseIterExt<K, V> for IndexMap<K, V>
+//where 
+    //K: 'static,
+    //V: 'static
+//{
+//impl<S: Hasher + BuildHasher + Default> HashMapReverseIterExt<String, String> for IndexMap<String, String, S> {
+impl HashMapReverseIterExt<String, String> for IndexMap<String, String> {
+    //fn rev_iter(self) -> IntoIter<String, String> {
+    fn rev_iter(self) -> Self {
+        self
+            .into_iter()
+            .map(
+                |(key, val)|
+                (val, key)
+                )
+            .collect()
+        }
+    }
+/*
+impl<K, V> HashMapReverseIterExt<K, V> for IndexMap<K, V> {
+    //type Item = (&'a V, &'a K);
+    //type ReverseIterator = indexmap::map::Iter<&'a Self::Item>;
+    //fn rev_iter(self) -> IndexMap<V, K> {
+    //fn rev_iter<'a> (self) -> Self::ReverseIterator
+    fn rev_iter<'a>(self) -> Iter<'a, V, K>
+        //where K: 'static,
+              //V: 'static
+    {
+        self
+            .into_iter()
+            .map(
+                |(key, val)|
+                (val, key)
+                )
+            .into_iter()
+            .collect()
+        //self.into_iter()
+        //self.iter().map(|(key, val)| {(val, key)}).collect()
+
+        //return self.iter() .map(|(key, val)| (val.to_owned(), key.to_owned()));
+
+        //let iter = self.iter()
+            //.map(|(key, val)| (val.to_owned(), key.to_owned()))
+            //.collect();
+        //return 
+    }
+}
+
+*/
+
+
+//pub trait HashMapReverseIterExt<A = Self> {
+    //fn rev_iter<I>(iter: I) -> Self
+        //where
+            //I: Iterator<Item = A>;
+//}
+
+
+//trait HashMapReverseIterExt<A = Self> {
+    //fn rev_iter<I>(iter: I) -> Self
+        //where
+            //I: Iterator<Item = A>;
+//}
+
+//impl<I: Iterator> HashMapReverseIterExt for  {
+    //fn rev_iter<I>(iter: I) -> Self
+            //where
+                //I: Iterator<Item = Self> {
+                    //return iter.map(|(key, val)| (val, key))
+        
+    //}
+//}
+
 fn main () {
     // Output to our src directory
     //env::set_var("OUT_DIR", "src");
@@ -89,8 +183,10 @@ fn main () {
     let nato_map: DataStore = serde_json::from_str(NATO_CONTS).unwrap();
 
     generate_resource(&mut file, "TO_NATO", nato_map.iter());
-    generate_resource(&mut file, "FROM_NATO", nato_map
-        .iter()
-        .map(|(key, val)| { (val, key) })
-        );
+    generate_resource(&mut file, "FROM_NATO", nato_map.rev_iter().iter());
+
+    //generate_resource(&mut file, "FROM_NATO", nato_map
+        //.iter()
+        //.map(|(key, val)| { (val, key) })
+        //);
 }
